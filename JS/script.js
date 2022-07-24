d0368e1a152614faa18a33d1d76e9cd406e6a9d1
kehindebillions <ogunyade.kehinde@gmail.com>
2022-07-24 14:44:13 +0100

    first commit

---
+++ JS/script.js
@@ -0,0 +1,179 @@
+// alert("Hello World");
+
+//console.log("hello world");// outputing and debugging
+
+// variables
+// declare a variables --> var let and const
+
+// var nam = "hello World";
+// var nam = "kenny"
+
+// var nows = "i am nows"
+// function home() {
+//     var nows = "hi"
+
+// }
+
+// console.log(nows)
+
+// console.log(nam)
+
+let colours = "black";
+let age = 20;
+
+//let colours = "blue";
+
+// if (age > 19) {
+//     abc = "hi"
+//      console.log(abc)
+//     } else
+//     abcd = "hello"
+
+//  console.log(abcd)
+
+//const nam = 'kenny'
+
+// Data Types
+const names = "kenny"; //string
+
+// const fruit = ["pear", 1, null, true, false]
+
+
+//Object in JS
+// const person = {
+//     name: "Eve",
+//     age: 5000,
+//     isPerson: true,
+//     hobbies: ["eating", "sleeping"]
+//     };
+
+//     //console.log(person); // key and value
+
+// function dummyFunc(){}
+
+// // Check type
+// console.log(typeof names);
+
+// let name = "Fola"
+// let age = 99
+
+// Archaic method
+//console.log('My name is ' + name + ' and I am ' + age);
+
+//Template literal (use this one!)
+//console.log(`My name is ${name} and I am ${4*6}`);
+
+//console.log('je m \n appelle wtvr')
+
+// STRING METHODS & PROPERTIES
+
+let s = "Hello World";
+let val;
+
+// Get length
+//val = s.length;
+
+//console.log(s[0]);
+// val = s.substring(0, 4);
+// val = s.split('');
+
+// const fruit = ['pear', 'orange', 'orange'];
+// val = fruit.join('+');
+// console.log(val);
+
+//console.log('3'**'2')
+// let y = 5
+// //console.log(--y)
+// console.log(y)
+
+// const person = { // an object is a collection of key-value pairS
+//     firstName: 'Adam',
+//     age: 30,
+//     hobbies: ['music', 'movies', 'sports'],
+//     address: {
+//     street: '66 Main st',
+//     city: 'wtvr',
+//     state: 'over there'
+//     }
+//     };
+
+//     // Get single value (obj.key)
+//     //console.log(person.firstName)
+
+//     // Add property
+//     person.email = 'adam@gmail.com';
+//     person.hobbies.push('singing');
+//     console.log(person);
+
+// const person = {
+//     name: 'foulan',
+//     age:27,
+//     adresse:'tunis'}
+
+//     // Change a property
+//     // person.name = 'mohamed'
+
+//     // // Delete a property
+//     //delete(person.age)
+
+//     // // Add a property
+//     person.email = 'm@gmail.com';
+
+//     console.log(person)
+
+// const numbers = [1,2,3]
+
+// numbers[1] = 'hello'
+
+// console.log(numbers)
+
+// let i = 1;
+// while (i <= 10) {
+// console.log(`While Loop Number: ${i}`);
+// i++;
+// }
+
+const numbers = [99, 70, 66];
+
+// // ********************** For Loop
+// for (let i = 0; i < numbers.length; i++) {
+// console.log(numbers[i]);
+// }
+
+// // ********************** For...of Loop
+// for (let x of numbers) {
+// console.log(x);
+// }
+
+// // // ********************** For...in Loop
+
+// for (let x in numbers) {
+// console.log(x);
+
+// }
+
+let color = "black";
+
+// switch (color) {
+// case 'red':
+// console.log('color is red');
+// break;
+// case 'blue':
+// console.log('color is blue');
+// break;
+// default:
+// console.log('color is neither red nor blue');
+// }
+
+// let colorQualified = color === "red" ? "is red" : "isn't red"; // condition ? statement1 : statement2 : statement3
+// console.log(`Color ${colorQualified}!`);
+
+// function greet(name) {
+//   console.log(`Hello my name is ${name}`);
+// }
+
+// console.log(greet("Kenny"));
+
+// const greet = name => { console.log(`Hello my name is ${name}`); };
+
+// greet('Adam');
